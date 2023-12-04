2b82ea8aa72277ef38ed758177a71d92e9af27dc
WonGi-Kim <kwg970310@gmail.com>
2023-12-04 14:56:35 +0900

    Initial Commit

---
+++ YouTubeMusicClone/YouTubeMusicCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouTubeMusicCloneApp.swift
+//  YouTubeMusicClone
+//
+//  Created by 김원기 on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct YouTubeMusicCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
